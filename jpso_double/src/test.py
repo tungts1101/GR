8673301cbf3dff999151e414ab1d5639b5eccec1
tung <tungts1101@gmail.com>
2019-05-19 00:01:49 +0700

    run on medium size data

--- jpso_double/src/test.py
+++ jpso_double/src/test.py
@@ -36,8 +36,8 @@ def draw():
         v1_s,t1_s = cal(small,Swarm_v1)
         v2_s,t2_s = cal(small,Swarm_v2)
 
-        #v1_m,t1_m = cal(medium,Swarm_v1)
-        #v2_m,t2_m = cal(medium,Swarm_v2)
+        v1_m,t1_m = cal(medium,Swarm_v1)
+        v2_m,t2_m = cal(medium,Swarm_v2)
     except Exception as ex:
         print(ex)
         exit(1)
@@ -54,11 +54,12 @@ def draw():
     
     print('|{:30}|{:^15.2f}|{:^14.2f}|{:^15.2f}|{:^14.2f}|'.format('Source & relay nodes = 40',v1_s,v2_s,t1_s,t2_s))
     
-    print('|{:92}|'.format(''.join(['_' for _ in range(92)])))
+    print('{:92}'.format(''.join(['_' for _ in range(92)])))
     
-    #print('|{:30}|{:^15.2f}|{:^14.2f}|{:^15.2f}|{:^14.2f}|'.format('Source & relay nodes = 100',v1_m,t1_m,v2_m,t2_m))
+    print('|{:30}|{:^15.2f}|{:^14.2f}|{:^15.2f}|{:^14.2f}|'.format('Source & relay nodes = 100',v1_m,v2_m,t1_m,t2_m))
+    
+    print('{:92}'.format(''.join(['_' for _ in range(92)])))
 
-    #print('|{:30}|{:61}|'.format(''.join(['_' for _ in range(30)]),''.join(['_' for _ in range(61)])))
 
     exit(0)
 
